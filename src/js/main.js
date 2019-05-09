b851116c91a6f327a30fe921cd002936eb1ef165
qqavon <qqavon@gmail.com>
2019-05-09 20:56:06 +0200

    Added click event on tech item to show name

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,35 @@ function showSection(index) {
             Array.from(sections[0].children).forEach((v,i) => {
                 setTimeout(() => { v.classList.remove('hidden') }, 100 * i)
             })
-            break;
+            break
         case 1:
-        document.querySelector('.aboutMe').classList.remove('hidden')
-        document.querySelectorAll('.description span').forEach((span,i) => {
-            setTimeout(() => { span.classList.remove('hidden') }, 125 * i)
-        })
+            sections[1].classList.remove('hidden')
+            document.querySelectorAll('.description span').forEach((span,i) => {
+                setTimeout(() => { span.classList.remove('hidden') }, 125 * i)
+            })
+            break
+        case 2:
+            sections[2].classList.remove('hidden')
+            document.querySelectorAll('.tech_list_item').forEach((v,i) => {
+                setTimeout(() => {
+                    v.classList.remove('hidden')
+                }, 75 * i)
+            })
+            break
+
     }
 }
+document.querySelectorAll('.tech_list_item').forEach((v,i) => {
+    let hidden = true
+
+    v.addEventListener('click', () => {
+        if(hidden) {
+            hidden = false
+            v.children[1].classList.remove('hidden')
+        }
+        else {
+            hidden = true
+            v.children[1].classList.add('hidden')
+        }
+    })
+})

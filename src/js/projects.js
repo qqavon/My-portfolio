76338a00cf4f96579bb1b1b445939310aa4eacb2
qqavon <qqavon@gmail.com>
2019-06-29 16:20:09 +0200

    Projects

---
+++ src/js/projects.js
@@ -0,0 +1,131 @@
+const projectsArr = [
+    {
+        name: "event page",
+        title: "Event page",
+        stack: ['vue'],
+        description: '<p> Strona prezentująca organizowane wydarzenie. Projekt powstał w celu praktycznej nauki Vue. </p>',
+        features: ["Responsywna", "Interaktywny plan wydarzeń", "Parallax", "Karuzela tekstu (na mniejszych ekranach)"],
+        gitHubUrl: 'https://github.com/qqavon/eventpage#event-page',
+        demoUrl: 'https://qqavon.github.io/eventpage'
+    },
+    {
+        name: "quizzes",
+        title: "Quizzes",
+        stack: ['vue', 'nodejs', 'mongodb'],
+        description: '<p> Użytkownik po rejestracji może tworzyć pytania które będą odpowiadane przez zalogowanych użytkowników. </p>',
+        features: ["Rejestracja i logowanie", "Dodawanie i usuwanie quizów", "Wyszukiwarka quizów", "Paginacja"],
+        gitHubUrl: 'https://github.com/qqavon/quizzes#quizzes',
+        demoUrl: 'https://qqavon.github.io/quizzes'
+    },
+    {
+        name: "newsletter",
+        title: "Newsletter",
+        stack: ['html5', 'css3', 'js'],
+        description: '<p> Formularz zawierający prostą walidację i animacje wykonane w canvasie. Kuleczki przemieszczają się szybciej wzdłóż osi Y podczas podawania e-malia, jeżeli jest błędny, będą się trząść. </p>',
+        features: ["Animacje zrobione w canvas API"],
+        gitHubUrl: 'https://github.com/qqavon/newsletter#newsletter',
+        demoUrl: 'https://qqavon.github.io/newsletter/'
+    },
+    {
+        name: "strona firmy",
+        title: "Strona firmy",
+        stack: ['vue'],
+        description: '<p> ... Strona wykonana w celu praktycznej nauki Vue </p>',
+        features: ["Responsywna", "Multi page", "Karuzela zdjeć", "Mapa", "Lazy loading komponentów"],
+        gitHubUrl: 'https://github.com/qqavon/stronaFirmy#strona-firmy',
+        demoUrl: 'https://qqavon.github.io/stronaFirmy/'
+    },
+    {
+        name: "portfolio",
+        title: "Portfolio",
+        stack: ['html5', 'css3', 'js'],
+        description: 'Portfolio które właśnie przeglądasz.',
+        features: ["Responsywna"],
+        gitHubUrl: 'https://github.com/qqavon/My-portfolio#portfolio',
+        demoUrl: 'https://qqavon.github.io/My-portfolio/'
+    },
+    {
+        name: "React posty i albumy",
+        title: "React posty i albumy",
+        stack: ['reactjs'],
+        description: 'Opis projektu React posty i albumy',
+        features: ["FICZER 1", "FICZER 2"],
+        gitHubUrl: '',
+        demoUrl: ''
+    },
+]
+
+const projectsElement = document.querySelectorAll('.projects__list__item')
+const selectedProjectElement = document.querySelector('.selected-project')
+const nextSelectedBtn = document.querySelector('#nextSelected')
+const prevSelectedBtn = document.querySelector('#prevSelected')
+const closeSelectedBtn = document.querySelector('#closeSelected')
+
+const selectedProjectTitle = document.querySelector('.selected-project__buttons__title')
+const selectedProjectStack = document.querySelector('.selected-project__stack__list')
+const selectedProjectDescription = document.querySelector('.selected-project__description__text')
+const selectedProjectFeatures = document.querySelector('.selected-project__features__list')
+const selectedProjectDemoUrl = document.querySelector('#selectedProjectDemoUrl')
+const selectedProjectGithubUrl = document.querySelector('#selectedProjectGithubUrl')
+
+let selectedProjectIndex = null
+let projectsQuantity = projectsElement.length
+
+//Po kliknieciu wyswietlane są informacje projektu
+projectsElement.forEach((project, index) => {
+    project.addEventListener('click', () => {
+        selectProject(index)
+    })
+})
+
+closeSelectedBtn.addEventListener('click', () => { closeSelectedProject() })
+nextSelectedBtn.addEventListener('click', () => { nextSelectedProject() })
+prevSelectedBtn.addEventListener('click', () => { prevSelectedProject() })
+
+function selectProject(index) {
+    const project = projectsElement[index]
+    selectedProjectIndex = index
+    const projectData = projectsArr.filter(v => v["name"] == project.getAttribute('data-project'))[0]
+
+    projectsElement.forEach(v => v.classList.remove('active'))
+    project.classList.add('active')
+
+    selectedProjectElement.classList.remove('hidden')
+
+    selectedProjectTitle.innerHTML = projectData.title
+    selectedProjectDescription.innerHTML = projectData.description
+
+    let stackHtml = ''
+    projectData.stack.forEach(logo => {
+        stackHtml += `<div class="selected-project__stack__list__item"> ${getLogo(logo)} </div>`
+    })
+    selectedProjectStack.innerHTML = stackHtml
+
+    let featuresHtml = ''
+    projectData.features.forEach(feature => {
+        featuresHtml += `<li class="selected-project__features__list__item"> ${feature} </li>`
+    })
+    selectedProjectFeatures.innerHTML = featuresHtml
+
+    console.log(projectData.demoUrl)
+    console.log(projectData.gitHubUrl)
+
+    selectedProjectDemoUrl.setAttribute('href', projectData.demoUrl)
+    selectedProjectGithubUrl.setAttribute('href', projectData.gitHubUrl)
+}
+
+function closeSelectedProject() {
+    projectsElement[selectedProjectIndex].classList.remove('active')
+    selectedProjectIndex = null
+    selectedProjectElement.classList.add('hidden')
+}
+
+function nextSelectedProject() {
+    const index = selectedProjectIndex+1 > projectsQuantity-1 ? 0 : selectedProjectIndex+1
+    selectProject(index)
+}
+
+function prevSelectedProject() {
+    const index = selectedProjectIndex-1 < 0 ? projectsQuantity-1 : selectedProjectIndex-1
+    selectProject(index)
+}
--- src/js/tech-and-tools.js
+++ src/js/tech-and-tools.js
@@ -3,85 +3,73 @@ const techAndToolsData = [
         name: "html",
         fullName: "HTML5",
         description: "Opis html",
-        isIcon: true,
-        iconClass: "devicon-html5-plain colored",
+        logo: "html5"
     },
     {
         name: "css",
         fullName: "CSS3",
         description: "Opis css",
-        isIcon: true,
-        iconClass: "devicon-css3-plain colored",
+        logo: "css3"
     },
     {
         name: "js",
         fullName: "JavaScript",
         description: "Opis js",
-        isIcon: true,
-        iconClass: "devicon-javascript-plain colored",
+        logo: "js"
     },
     {
         name: "nodejs",
         fullName: "NodeJs",
-        description: "Opis nodejs",
-        isIcon: true,
-        iconClass: "devicon-nodejs-plain colored",
+        description: "Opis nodejs Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus molestias dignissimos doloremque aspernatur nobis hic, distinctio dolor similique libero error, porro est quis! Impedit excepturi ex amet laboriosam, sint error nobis fugiat vel, quam eveniet, et nostrum. Totam excepturi reprehenderit pariatur illum eligendi doloribus saepe doloremque quia, ex cum accusantium.",
+        logo: "nodejs"
     },
     {
         name: "mongodb",
         fullName: "MongoDB",
         description: "Opis mongodb",
-        isIcon: true,
-        iconClass: "devicon-mongodb-plain colored",
+        logo: "mongodb"
     },
     {
         name: "vue",
         fullName: "Vue",
         description: "Opis vue",
-        isIcon: true,
-        iconClass: "devicon-vuejs-plain colored",
+        logo: "vue"
     },
     {
         name: "sass",
         fullName: "Sass",
         description: "Opis sass",
-        isIcon: true,
-        iconClass: "devicon-sass-original colored",
+        logo: "sass"
     },
     {
         name: "react",
         fullName: "ReactJs",
         description: "Opis reactjs",
-        isIcon: true,
-        iconClass: "devicon-react-original colored",
+        logo: "reactjs"
     },
     {
         name: "vscode",
         fullName: "Visual Studio Code",
         description: "Opis vscode",
-        isIcon: false,
-        img: "./src/img/vscode.png"
+        logo: "vscode"
     },
     {
         name: "git",
         fullName: "Git",
         description: "Opis git",
-        isIcon: false,
-        img: "./src/img/git.png"
+        logo: "git"
     },
     {
         name: "gimp",
         fullName: "Gimp",
         description: "Opis gimp",
-        isIcon: false,
-        img: "./src/img/gimp.png"
+        logo: "gimp"
     },
     {
         name: "adobexd",
-        fullName: "AdobeXd",
+        fullName: "Adobe Experience Design",
         description: "Opis adobexd",
-        isIcon: false,
-        img: "./src/img/adobexd.png"
+        logo: "adobexd"
     },
 ]
 
@@ -90,34 +78,24 @@ const techSelectedName = document.querySelector('.tech-and-tools__container__sel
 const techSelectedLogo = document.querySelector('.tech-and-tools__container__selected__logo')
 const techSelectedDescription = document.querySelector('.tech-and-tools__container__selected__description')
 
-//Pobieranie technologii i narzedzi
 const techItems = document.querySelectorAll('.tech-and-tools__list__item')
 
 //Dodawanie click event do tech./narz. w celu wyswietlenia informacji wybranej technologii
 //i przełączanie klasy dodająca styl
 techItems.forEach(item => {
     item.addEventListener('click', () => {
-        //Usuwanie i dodawanie klasy do wybranej technologii/narzedzia
+        const techName = item.getAttribute("data-tech")
+        const techData = techAndToolsData.filter(item => item.name == techName)[0]
+
+        const { fullName, logo, description } = techData
+
         techItems.forEach(item => item.classList.remove('selected'))        
         item.classList.add('selected')
 
-        //Wyciąganie danych wybranej technologii/narzędzia
-        const techName = item.getAttribute("data-tech")
-        const techData = techAndToolsData.filter(item => item.name == techName)[0]
         
-        //Nazwa technologii/narzedzia
-        techSelectedName.innerHTML = techData["fullName"]
-
-        //Ikona/zdjęcie technologii/narzędzia
-        if(techData["isIcon"]) {
-            techSelectedLogo.innerHTML = `<i class="${techData["iconClass"]}"></i>`
-        }
-        else {
-            techSelectedLogo.innerHTML = `<img src="${techData["img"]}" alt="git icon">`
-        }
-
-        //Opis
-        techSelectedDescription.innerHTML = techData["description"]
+        techSelectedName.innerHTML = fullName
+        techSelectedLogo.innerHTML = getLogo(logo)
+        techSelectedDescription.innerHTML = description
     })
 })
 

981bcb9191c63f39982b0f105ad0cee7f4d7e7d7
qqavon <qqavon@gmail.com>
2019-08-19 17:57:00 +0200

    Added technologies, tools and scale transition

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,14 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import './skills.css'
+import '../../addons/css/scaleTransition.css'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 const Skills = () => {
+
+    const [ iconSrc, setIconSrc ] = useState( '' )
+    const technologies = [
+        { name: 'HTML5', icon: 'html5.svg' },
+        { name: 'CSS3', icon: 'css3.svg' },
+        { name: 'JavaScript', icon: 'javascript.svg' },
+        { name: 'NodeJs', icon: 'nodejs.svg' },
+        { name: 'MongoDB', icon: 'mongodb.svg' },
+        { name: 'Vue', icon: 'vuejs.svg' },
+        { name: 'Sass', icon: 'sass.svg' },
+        { name: 'React', icon: 'reactjs.svg' },
+    ]
+    const tools = [
+        { name: 'Visual Studio Code', icon: 'vscode.png' },
+        { name: 'Git', icon: 'git.png' },
+        { name: 'Adobe Experience Design', icon: 'adobexd.png' }
+    ]
+
+    const onHover = item => {
+        setIconSrc( item.icon )
+    }
+
     return (
         <section id="skills">
             <h1 className="section-name">
                 Umiejętności
                 <span className="section-name__background-text"> 2. </span>
             </h1>
-            <Link to="/" > Back </Link>
+            <div className="skills__body">
+                <div className="skills__container">
+                    <span className="skills__container__name"> Technologie </span>
+                    <div className="skills__container__list">
+                        { technologies.map((tech, index) => (
+                            <span key={ index } onMouseOver={ () => onHover(tech) } className="skills__container__list__item">
+                                { tech.name }
+                            </span>
+                        )) }
+                    </div>
+                </div>
+
+                <div className="skills__container">
+                <span className="skills__container__name"> Narzędzia </span>
+                    <div className="skills__container__list">
+                    { tools.map((tool, index) => (
+                        <span key={ index } onMouseOver={ () => onHover(tool) } className="skills__container__list__item">
+                            { tool.name }
+                        </span>
+                    )) }
+                    </div>
+                </div>
+                <div className="skills__body__icon">
+                    { iconSrc ?
+                    <TransitionGroup>
+                        <CSSTransition
+                            key={ iconSrc }
+                            timeout={ 200 }
+                            classNames="scale"
+                        >
+                            <img src={`src/img/${iconSrc}`} />
+                        </CSSTransition>
+                    </TransitionGroup>
+                    : null }
+                </div>
+            </div>
         </section>
     )
 }

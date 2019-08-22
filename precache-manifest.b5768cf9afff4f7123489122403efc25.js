12c9054d71d1815bd3fd19390e3bb92d77d0bd65
qqavon <qqavon@gmail.com>
2019-08-22 13:50:03 +0200

    Updates

---
+++ precache-manifest.b5768cf9afff4f7123489122403efc25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd1343152519374c889363235f1f035",
+    "url": "/My-portfolio/index.html"
+  },
+  {
+    "revision": "0e2091ba57b928388601",
+    "url": "/My-portfolio/static/css/main.01995e0f.chunk.css"
+  },
+  {
+    "revision": "357c5068c018df79dbc6",
+    "url": "/My-portfolio/static/js/2.137f30ee.chunk.js"
+  },
+  {
+    "revision": "0e2091ba57b928388601",
+    "url": "/My-portfolio/static/js/main.fab595c1.chunk.js"
+  },
+  {
+    "revision": "e8b3e8eb68a22d4954bf",
+    "url": "/My-portfolio/static/js/runtime~main.82d73706.js"
+  }
+]);
